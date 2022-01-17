6deba0dfff4b416187e49056a0eb85dffeb9c3d5
Buğrahan Çamlıbel <camlibelbugrahan@gmail.com>
2022-01-17 23:32:33 +0300

    debugShowCheckedModeBanner: false

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: _title,
       theme: ThemeData(
         brightness: Brightness.light,
