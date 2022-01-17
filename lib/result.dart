88c28034c6457319e55b301d0591852683a56701
Buğrahan Çamlıbel <camlibelbugrahan@gmail.com>
2022-01-17 17:11:59 +0300

    Visual improvements.

--- lib/result.dart
+++ lib/result.dart
@@ -16,7 +16,6 @@ class ResultWidget extends StatelessWidget {
           itemBuilder: (BuildContext context, int index){
             return BankOffer(index: index, bankOffer: responseData.offers[index],);
           },
-
         ),
       )
     );
@@ -30,43 +29,53 @@ class BankOffer extends StatelessWidget {
   final Offer bankOffer;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(bankOffer.bank,style: const TextStyle(fontSize: 18, fontStyle: FontStyle.italic),),
-        GestureDetector(
-
-          onTap: (){
-            print("Clicked on it");
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    const Text("ORAN"),
-                    Text(bankOffer.rate.toString()),
-                  ],
-                ),
-                Column(
-                  children: [
-                    const Text("YILLIK GİDER"),
-                    Text(bankOffer.annualExpenseRate.toString()),
-                  ],
-                ),
-                Column(
-                  children: [
-                    const Text("YILLIK GİDER"),
-                    Text(bankOffer.annualExpenseRate.toString()),
-                  ],
-                ),
-              ],
-            ),
+    return GestureDetector(
+      onTap: (){
+        print("Clicked on it");
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFFEFEFEF),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.all(Radius.circular(20))
           ),
-        )
-      ],
+          //color: Colors.grey,
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(bankOffer.bank,style: const TextStyle(fontSize: 18, fontStyle: FontStyle.italic),),
+              Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          const Text("ORAN"),
+                          Text(bankOffer.rate.toString()),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text("YILLIK GİDER"),
+                          Text(bankOffer.annualExpenseRate.toString()),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text("YILLIK GİDER"),
+                          Text(bankOffer.annualExpenseRate.toString()),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
